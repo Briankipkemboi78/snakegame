893c6973dd9cb7b444a10a32c44375d250f90834
Brian <bkipkemboi@watuafrica.co.ke>
2023-11-20 18:08:19 +0300

    reformat file

--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 
-STARTING_POSITION = [(0,0), (-20,0), (-40,0)]
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+
 
 class Snake:
 
@@ -21,8 +22,8 @@ class Snake:
         self.segments.append(new_segment)
 
     def move(self):
-        for seg_num in range(len(self.segments)-1, 0, -1):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
-            self.segments[seg_num].goto(new_x,new_y)
+            self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(20)
